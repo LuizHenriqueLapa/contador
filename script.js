37f5e355a72936bd90adb681b31f02d1d9347041
luiz <99219278+LuizHenriqueLapa@users.noreply.github.com>
2022-02-14 19:59:37 -0300

    criando contador back

---
+++ script.js
@@ -0,0 +1,18 @@
+let contador = document.querySelector('p')//pega o p para transformarlo numa variavel
+
+console.log(contador)//olho magico
+
+function less(){// a funcáo
+
+    --contador
+console.log(contador)
+
+}
+
+function more(){
+
+++contador
+console. log(contador)
+
+
+}
